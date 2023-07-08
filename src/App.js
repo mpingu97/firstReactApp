f960bd6cf0d78166fa29b6d5d61688f78d1c7604
Pingu97 <milanw.1997@gmail.com>
2023-07-08 17:37:22 +0200

    Added new component to app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import ExpanseItem from "./components/ExpanseItem";
+
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
+      <ExpanseItem></ExpanseItem>
     </div>
   );
 }
