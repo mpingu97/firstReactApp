aba1090e9f9dbc330f06d580d0ede75e863ed45f
Pingu97 <milanw.1997@gmail.com>
2023-07-09 22:26:39 +0200

    Added clean date style

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,17 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem(probs) {
+  const month = probs.date.toLocaleString("en-US", { month: "long" });
+  const day = probs.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = probs.date.getFullYear();
+
   return (
     <div className="expense-item">
-      <div> {probs.date.toISOString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div className="expense-item__desciption">
         <h2> {probs.title} </h2>
         <div className="expense-item__price">€{probs.amount}</div>
